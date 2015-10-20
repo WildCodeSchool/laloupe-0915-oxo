e8601e2cfc5ba715473f10521220430df043b8a5
Rabin Julien <julienr114@gmail.com>
2015-10-20 14:47:57 +0200

    cherchage de jeux ok

--- public/js/controllers/list_vgController.js
+++ public/js/controllers/list_vgController.js
@@ -1,20 +1,12 @@
 // LIST VIDEOS GAMES CONTROLLER
 function list_vgController($scope, list_vgService) {
-    $scope.loadingInProgress = true;
+    $scope.loadingInProgress = false;
     $scope.get = function(){
+        $scope.loadingInProgress = true;
         list_vgService.gameSearch($scope.jeux, function(data){ 
             $scope.games = data;
-            $scope.results = ' résultat(s) à votre recherche'; 
-        }).then(function(res) { 
-                // My success function
-                $scope.games = data; 
-                $scope.loadingInProgress = false;
-                },
-            function() { 
-                // My failure function
-                $scope.games != data;
-                $scope.loadingInProgress = false; 
-});;
+            $scope.loadingInProgress = false;
+        });
     }
     
 }
