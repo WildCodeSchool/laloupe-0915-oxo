70fef0f1c2d2d9533fed05b5abe169d6b8e04256
Rabin Julien <julienr114@gmail.com>
2015-10-20 11:09:37 +0200

    recherche des jeux fonctionnelle

--- public/js/controllers/list_vgController.js
+++ public/js/controllers/list_vgController.js
@@ -1,11 +1,22 @@
 // LIST VIDEOS GAMES CONTROLLER
 function list_vgController($scope, list_vgService) {
+    $scope.loadingInProgress = true;
     $scope.get = function(){
-        list_vgService.gameSearch($scope.jeux, function(data){
-            console.log(data);  
+        list_vgService.gameSearch($scope.jeux, function(data){ 
             $scope.games = data;
-        });
+            $scope.results = ' résultat(s) à votre recherche'; 
+        }).then(function(res) { 
+                // My success function
+                $scope.games = data; 
+                $scope.loadingInProgress = false;
+                },
+            function() { 
+                // My failure function
+                $scope.games != data;
+                $scope.loadingInProgress = false; 
+});;
     }
+    
 }
 
 	
