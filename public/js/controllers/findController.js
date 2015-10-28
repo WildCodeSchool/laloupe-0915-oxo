4d07cf8d053f244d3ff15fc483d1648226a13a83
Arano <etsaiak2@hotmail.fr>
2015-10-28 11:50:52 +0100

    petit changement

--- public/js/controllers/findController.js
+++ public/js/controllers/findController.js
@@ -1,15 +1,15 @@
 // FIND CONTROLLER 
 function findController($scope, findService){
 
-    function initMap() {
-      // Create a map object and specify the DOM element for display.
-      var map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -34.397, lng: 150.644},
-        scrollwheel: false,
-        zoom: 8
-      });
-    }
 
-    initMap();
+var map = L.map('map').setView([48.4712850, 1.0143050], 17);
+
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([48.4712850, 1.0143050]).addTo(map)
+    .bindPopup('Oxo! <br> You are the winer!')
+    .openPopup();
+
 }
-
