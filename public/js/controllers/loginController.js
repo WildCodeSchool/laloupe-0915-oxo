3621b47a7804de8e4741afb33b3efe9e1e84aa78
mekaVR <candau.herbert@gmail.com>
2015-10-29 22:29:16 +0100

    fix bug: retablisement de la route pour s'inscrire

--- public/js/controllers/loginController.js
+++ public/js/controllers/loginController.js
@@ -13,32 +13,11 @@ function loginController($scope, $http, $rootScope, loginService, $location) {
 		});
 	}
 
-	/*$scope.send = function(){
-		var data = {};
-		data.pseudo = $scope.pseudo;
-		data.mdp = $scope.mdp;
-
-		
-
-		loginService.post(data).then(function(res){
-			console.log(res.data);
-			if (!res.data){
-				alert("Vous n'êtes pas encore incrit !");
-				$location.path('/');
-			}
-
-			else {
-				$rootScope.user = res.data;
-				$location.path('/home');
-			}
-		});
-	}
-
 	$scope.register = function(){
 
 
 		$location.path('/formulaire');
 
-	}*/
+	}
 }
 
